745feb1f49ccba7787f0d332e5d24834e2b1e340
Xavierlins <xlins4255@gmail.com>
2026-03-30 09:59:15 -0300

    Fiz algumas alterações #TamoJuntoNaLua

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const botao = document.querySelector("#btnEnviar");
-botao.addEventListener("click", function(){
-    alert("Botão clicado!");
-});
+const btnEnviar = document.querySelector("#btnEnviar");
+
+const btnCancelar = document.querySelector("#btnCancelar");
+
+if (btnEnviar) {
+    btnEnviar.addEventListener("click", function() {
+        alert(" Sucesso! Sua solicitação foi enviada.");
+    });
+}
+
+if (btnCancelar) {
+    btnCancelar.addEventListener("click", function() {
+        const confirmacao = confirm("Tem certeza que deseja cancelar?");
+        if (confirmacao) {
+            alert(" Operação cancelada.");
+        }
+    });
+}
+
