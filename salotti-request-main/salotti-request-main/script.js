5e9705ff62f07f8b4c993b0f72dca4ac9d427639
Manu <emanuellahsilva2020@gmail.com>
2026-04-07 08:30:53 -0300

    Manu

--- salotti-request-main/salotti-request-main/script.js
+++ salotti-request-main/salotti-request-main/script.js
@@ -1,6 +1,31 @@
-const botao = document.querySelector("#btnEnviar");
 
-botao.addEventListener("click", function() {
-    console.log("%c Transmissão Iniciada...", "color: #00f2ff; font-weight: bold;");
-    alert("Acesso autorizado. Sistema carregando...");
+const btnPetisco = document.getElementById('btnEnviar');
+const btnSoneca = document.getElementById('btnExcluir');
+
+
+btnPetisco.addEventListener('click', () => {
+   
+    const originalText = btnPetisco.innerHTML;
+    btnPetisco.innerHTML = "";
+    
+    btnPetisco.style.transform = 'scale(1.1)';
+
+    setTimeout(() => {
+        btnPetisco.innerHTML = originalText;
+        btnPetisco.style.transform = 'scale(1)';
+        alert('Ok');
+    }, 400);
+});
+
+
+btnSoneca.addEventListener('click', () => {
+    const originalText = btnSoneca.innerHTML;
+    btnSoneca.innerHTML = "";
+    btnSoneca.style.opacity = '0.7';
+
+    setTimeout(() => {
+        btnSoneca.innerHTML = originalText;
+        btnSoneca.style.opacity = '1';
+        alert('Erro');
+    }, 1000); 
 });
